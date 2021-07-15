15ee3d1c0656e266a7ddb583725eae89bc56f7be
venkatajayadeep <jayadeep101097@gmail.com>
2021-07-15 15:35:31 +0530

    Added : Till_Month_Reached

--- EmpWageCompition.java
+++ EmpWageCompition.java
@@ -4,41 +4,47 @@ public class EmpWageCompition {
 
 	public static void main(String[] args) {
 		System.out.println("WelCome");	
-		int fullDayHr = 8;
-		int partTimeHr = 4;
-		int wagePerHr = 20;
-		int day = 0;
-		int empHr = 0;
+		int fullDayHr=8;
+		int partTimeHr=4;
+		int wagePerHr=20;
+		int day=0;
+		int empHr=0;
 		int wage = 0;
 
 		while (day <= 19) {
 
-			int caseCheck = (int) (Math.random() * 10) % 3;
+		int caseCheck = (int) (Math.random() * 10) % 3;
 
 		switch (caseCheck) {
 
 		case 1:
-			System.out.println("Present fullday");
-			wage = (wage + (wagePerHr * fullDayHr));
-			day++;
+				System.out.println("Present fullday");
+				wage = (wage + (wagePerHr * fullDayHr));
+            empHr = (empHr + fullDayHr);
+			 day++;
 			break;
 
 		case 2:
-			System.out.println("Present PartTime");
-			wage = (wage + (wagePerHr * partTimeHr));
-			day++;
+				System.out.println("Present PartTime");
+			   wage = (wage + (wagePerHr * partTimeHr));
+			   empHr = (empHr + partTimeHr);
+			 day++;
 			break;
 
 		default:
-			System.out.println("Absent");
+				System.out.println("Absent");
 		    day++;
-		}
-   		 }
 
-		System.out.println("total wage of a month is");
+			if( empHr == 100)
+				break;
+		}
+			 }
+	
+		System.out.println("Total wage of a month is");
 		System.out.println(wage);
+		System.out.println(empHr);
+		System.out.println(day);
 
 	}
 }
-
 	
