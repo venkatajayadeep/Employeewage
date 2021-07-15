80eaa472a50d441c590b4bc88b785e77cd088a35
venkatajayadeep <jayadeep101097@gmail.com>
2021-07-15 15:16:12 +0530

    Added: Present_Or_Absent

--- EmpWageCompition.java
+++ EmpWageCompition.java
@@ -4,7 +4,23 @@ public class EmpWageCompition {
 
 	public static void main(String[] args) {
 		System.out.println("WelCome");
+		
+			
+			int isFullTime = 1;
+			double empCheck = Math.floor(Math.random()*10)%2;
 
+			if(empCheck == isFullTime)
+			{
+				System.out.println("Employee is Present");
+			}
+			else
+			{
+				System.out.println("Employee is Abscent");
+			}
+			
+		}
+		
+	
 	}
 
-}
+
