f2cfd522a9a18420ec47251edcc2fe731426b632
venkatajayadeep <jayadeep101097@gmail.com>
2021-07-15 15:24:58 +0530

    Added : Part_Time

--- EmpWageCompition.java
+++ EmpWageCompition.java
@@ -3,27 +3,37 @@ package bridgelabz;
 public class EmpWageCompition {
 
 	public static void main(String[] args) {
-		System.out.println("WelCome");
-		
-			
+		System.out.println("WelCome");	
 		int isFullTime = 1;
-		int WagePerHr = 20;
-		int FullDayHr = 8;
-		
-		double empCheck = Math.floor(Math.random()*10)%2;
+		int isPartTime = 2;
+		int wagePerHr = 20;
+		int fullDayHr = 8;
+		int partTimeHr = 4;
+		int dailyEmpWage =0;
+
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 
 		if(empCheck == isFullTime)
 		{
 			System.out.println("Employee is Present");
-			int DailyEmpWage = WagePerHr * FullDayHr;
-			System.out.println("DailyEmpWage::" + DailyEmpWage);
+			dailyEmpWage = wagePerHr * fullDayHr;
+			System.out.println("DailyEmpWage of FullTime Employee::" + dailyEmpWage);
+
+
 		}
-		else
+		else if(empCheck == isPartTime)
+		{
+			System.out.println("Employee is Present");
+			dailyEmpWage = wagePerHr * partTimeHr;
+			System.out.println("Daily Wage of PartTime Employee::" + dailyEmpWage);
+		}else
 		{
 			System.out.println("Employee is Absent");
+
 		}
-		
 	}
-	
 }
 
+
+
+	
