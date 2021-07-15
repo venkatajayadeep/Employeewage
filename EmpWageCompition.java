7e8ca99eae287b06c33aec06c2a1048b85a8d3c1
venkatajayadeep <jayadeep101097@gmail.com>
2021-07-15 15:31:47 +0530

    Added : For_A_Month

--- EmpWageCompition.java
+++ EmpWageCompition.java
@@ -4,30 +4,41 @@ public class EmpWageCompition {
 
 	public static void main(String[] args) {
 		System.out.println("WelCome");	
-		 int caseCheck = (int)(Math.random() * 10) % 3;
-		 int fullDayHr = 8;
-		 int partTimeHr = 4;
-		 int wagePerHr = 20;
+		int fullDayHr = 8;
+		int partTimeHr = 4;
+		int wagePerHr = 20;
+		int day = 0;
+		int empHr = 0;
+		int wage = 0;
 
-		switch(caseCheck) {
+		while (day <= 19) {
+
+			int caseCheck = (int) (Math.random() * 10) % 3;
+
+		switch (caseCheck) {
 
 		case 1:
-			System.out.println("present FullDay ");
-			System.out.println(wagePerHr * fullDayHr);
+			System.out.println("Present fullday");
+			wage = (wage + (wagePerHr * fullDayHr));
+			day++;
 			break;
 
 		case 2:
-			System.out.println("present PartTime");
-			System.out.println(wagePerHr * partTimeHr);
+			System.out.println("Present PartTime");
+			wage = (wage + (wagePerHr * partTimeHr));
+			day++;
 			break;
 
 		default:
-			System.out.println("absent");
-		break;
-
+			System.out.println("Absent");
+		    day++;
 		}
+   		 }
+
+		System.out.println("total wage of a month is");
+		System.out.println(wage);
+
 	}
 }
-
 
 	
