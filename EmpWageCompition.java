bcaf65e7b4c97076aa3d33764f1c4b9cefd8b39e
venkatajayadeep <jayadeep101097@gmail.com>
2021-07-15 15:20:13 +0530

    Added : Daily_emp_Wage

--- EmpWageCompition.java
+++ EmpWageCompition.java
@@ -6,21 +6,24 @@ public class EmpWageCompition {
 		System.out.println("WelCome");
 		
 			
-			int isFullTime = 1;
-			double empCheck = Math.floor(Math.random()*10)%2;
+		int isFullTime = 1;
+		int WagePerHr = 20;
+		int FullDayHr = 8;
+		
+		double empCheck = Math.floor(Math.random()*10)%2;
 
-			if(empCheck == isFullTime)
-			{
-				System.out.println("Employee is Present");
-			}
-			else
-			{
-				System.out.println("Employee is Abscent");
-			}
-			
+		if(empCheck == isFullTime)
+		{
+			System.out.println("Employee is Present");
+			int DailyEmpWage = WagePerHr * FullDayHr;
+			System.out.println("DailyEmpWage::" + DailyEmpWage);
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
 		}
 		
-	
 	}
-
+	
+}
 
