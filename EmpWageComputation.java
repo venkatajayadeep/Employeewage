a863fc01ad9a94e7f7b442581e3b306b40fef682
venkatajayadeep <jayadeep101097@gmail.com>
2021-07-24 09:13:49 +0530

    Added : Multiple Compines

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,55 +1,17 @@
 package bridgelabz;
 
 public class EmpWageComputation {
-    static final int FULLDAYHR = 8;
-    static final int PARTTIMEHR = 4;
-    static final int WAGEPERHR = 20;
-    int day = 0;
-    int empTotalHour = 0;
-    int totalMonthWage = 0;
-    static int caseCheck;
+	 public static void main(String[] args) {
+	        WageCalculator mahindra = new WageCalculator();
+	        WageCalculator tata = new WageCalculator();
+	        WageCalculator kia = new WageCalculator();
 
-    public static int dailyCheck() {
-        caseCheck = (int) (Math.random() * 10) % 3;
-        return caseCheck;
-    }
-
-    public void monthWageCalculator() {
-        while (day <= 19) {
-            dailyCheck();
-            switch (caseCheck) {
-
-                case 1:
-                    System.out.println("Present fullday");
-                    totalMonthWage = (totalMonthWage + (WAGEPERHR * FULLDAYHR));
-                    empTotalHour = (empTotalHour + FULLDAYHR);
-                    day++;
-                    break;
-
-                case 2:
-                    System.out.println("Present PartTime");
-                    totalMonthWage = (totalMonthWage + (WAGEPERHR * PARTTIMEHR));
-                    empTotalHour = (empTotalHour + PARTTIMEHR);
-                    day++;
-                    break;
-
-                default:
-                    System.out.println("Absent");
-                    day++;
-
-                    if (empTotalHour == 100)
-                        break;
-            }
-        }
-        System.out.println("Total wage of a month :: " + totalMonthWage);
-        System.out.println("Total hours of employee :: " + empTotalHour);
-        System.out.println("Total days :: " + day);
-    }
-
-    public static void main(String[] args) {
-        EmpWageComputation ewcObj = new EmpWageComputation();
-        ewcObj.monthWageCalculator();
-    }
-
+	        System.out.println("Employee Wage For Mahindra :: ");
+	        mahindra.monthWageCalculator(350, 25, 125);
+	        System.out.println("Employee Wage For Tata :: ");
+	        tata.monthWageCalculator(400, 23, 120);
+	        System.out.println("Employee Wage For Kia :: ");
+	        kia.monthWageCalculator(500, 22, 100);
+	    }
 }
 	
